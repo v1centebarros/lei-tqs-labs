7b79e11aab31084e0c754918574a9c04ab9a61f0
v1centebarros <vmabarros@ua.pt>
2023-03-04 16:41:44 +0000

    Car Controller implemented

--- lab3/lab3_2CarService/src/main/java/pt/ua/deti/tqs/lab3_2carservice/boundary/CarController.java
+++ lab3/lab3_2CarService/src/main/java/pt/ua/deti/tqs/lab3_2carservice/boundary/CarController.java
@@ -1,25 +1,40 @@
 package pt.ua.deti.tqs.lab3_2carservice.boundary;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import pt.ua.deti.tqs.lab3_2carservice.data.Car;
 import pt.ua.deti.tqs.lab3_2carservice.service.CarManagerService;
 
 import java.util.List;
 
+@RestController
+@RequestMapping("/api")
 public class CarController {
 
-    private CarManagerService carManagerService;
+    private final CarManagerService carManagerService;
 
 
+    public CarController(CarManagerService carManagerService) {
+        this.carManagerService = carManagerService;
+    }
+
+    @PostMapping("/cars")
     public ResponseEntity<Car> createCar(Car car) {
-        return null;
+        HttpStatus status = HttpStatus.CREATED;
+        Car createdCar = this.carManagerService.createCar(car);
+        return new ResponseEntity<>(createdCar, status);
     }
 
+    @GetMapping("/cars")
     public List<Car> getAllCars() {
-        return null;
+        return this.carManagerService.getAllCars();
     }
 
-    public ResponseEntity<Car> getCarById(long id) {
-        return null;
+    @GetMapping("/cars/{id}")
+    public ResponseEntity<Car> getCarById(@PathVariable long id) {
+        Car car = this.carManagerService.getCarDetails(id).orElse(null);
+        HttpStatus status = car == null ? HttpStatus.NOT_FOUND : HttpStatus.OK;
+        return new ResponseEntity<>(car, status);
     }
 }
--- lab3/lab3_2CarService/src/main/java/pt/ua/deti/tqs/lab3_2carservice/service/CarManagerService.java
+++ lab3/lab3_2CarService/src/main/java/pt/ua/deti/tqs/lab3_2carservice/service/CarManagerService.java
@@ -1,22 +1,14 @@
 package pt.ua.deti.tqs.lab3_2carservice.service;
 
 import pt.ua.deti.tqs.lab3_2carservice.data.Car;
-import pt.ua.deti.tqs.lab3_2carservice.data.CarRepository;
 import java.util.List;
 import java.util.Optional;
 
-public class CarManagerService {
-    private CarRepository carRepository;
+public interface CarManagerService {
 
-    public Car createCar(Car car) {
-        return null;
-    }
+    Car createCar(Car car);
 
-    public List<Car> getAllCars() {
-        return null;
-    }
+    List<Car> getAllCars();
 
-    public Optional<Car> getCarDetails(long id) {
-        return Optional.empty();
-    }
+    Optional<Car> getCarDetails(long id);
 }
