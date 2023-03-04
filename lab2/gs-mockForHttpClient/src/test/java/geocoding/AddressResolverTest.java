8755b4c2b4fdac21f48a0a4478007d8d9d2cece7
v1centebarros <vmabarros@ua.pt>
2023-03-04 16:23:50 +0000

    Add lab 03 exercise 1

--- lab2/gs-mockForHttpClient/src/test/java/geocoding/AddressResolverTest.java
+++ lab2/gs-mockForHttpClient/src/test/java/geocoding/AddressResolverTest.java
@@ -28,14 +28,14 @@ class AddressResolverTest {
 
     @Test
     void whenResolveDetiGps_returnJacintoMagalhaeAddress() throws ParseException, IOException, URISyntaxException {
-        String longitude = "40.64143694391066";
-        String latitude = "-8.660728660686342";
+        double latitude = 40.64143694391066;
+        double longitude =  -8.660728660686342;
 
         String response = "{\"info\":{\"statuscode\":0,\"copyright\":{\"text\":\"© 2022 MapQuest, Inc.\",\"imageUrl\":\"http://api.mqcdn.com/res/mqlogo.gif\",\"imageAltText\":\"© 2022 MapQuest, Inc.\"},\"messages\":[]},\"options\":{\"maxResults\":1,\"ignoreLatLngInput\":false},\"results\":[{\"providedLocation\":{\"latLng\":{\"lat\":40.634159,\"lng\":-8.65795}},\"locations\":[{\"street\":\"Avenida João Jacinto de Magalhães\",\"adminArea6\":\"Aveiro\",\"adminArea6Type\":\"Neighborhood\",\"adminArea5\":\"Aveiro\",\"adminArea5Type\":\"City\",\"adminArea4\":\"Aveiro\",\"adminArea4Type\":\"County\",\"adminArea3\":\"\",\"adminArea3Type\":\"State\",\"adminArea1\":\"PT\",\"adminArea1Type\":\"Country\",\"postalCode\":\"3810-149\",\"geocodeQualityCode\":\"B1AAA\",\"geocodeQuality\":\"STREET\",\"dragPoint\":false,\"sideOfStreet\":\"N\",\"linkId\":\"0\",\"unknownInput\":\"\",\"type\":\"s\",\"latLng\":{\"lat\":40.63416,\"lng\":-8.65795},\"displayLatLng\":{\"lat\":40.63416,\"lng\":-8.65795},\"mapUrl\":\"\",\"roadMetadata\":{\"speedLimitUnits\":\"mph\",\"speedLimit\":31,\"tollRoad\":null}}]}]}";
 
         Mockito.when(client.doHttpGet(anyString())).thenReturn(response);
 
-        Optional<Address> address = resolver.findAddressForLocation(Double.parseDouble(latitude), Double.parseDouble(longitude));
+        Optional<Address> address = resolver.findAddressForLocation(latitude, longitude);
 
         assertTrue(address.isPresent());
         assertEquals("Avenida João Jacinto de Magalhães", address.get().getRoad());
@@ -44,16 +44,32 @@ class AddressResolverTest {
 
     @Test
     public void whenBadCoordidates_thenReturnNoValidAddress() throws IOException, URISyntaxException, ParseException {
-        String longitude = "420";
-        String latitude = "69";
+        double latitude = 420;
+        double longitude = 69;
 
         String response = "{\"info\":{\"statuscode\":400,\"copyright\":{\"text\":\"© 2022 MapQuest, Inc.\",\"imageUrl\":\"http://api.mqcdn.com/res/mqlogo.gif\",\"imageAltText\":\"© 2022 MapQuest, Inc.\"},\"messages\":[\"Illegal argument from request: Invalid LatLng specified.\"]},\"options\":{\"maxResults\":1,\"ignoreLatLngInput\":false},\"results\":[{\"providedLocation\":{},\"locations\":[]}]}";
 
         Mockito.when(client.doHttpGet(anyString())).thenReturn(response);
 
-        Optional<Address> address = resolver.findAddressForLocation(Double.parseDouble(latitude), Double.parseDouble(longitude));
+        Optional<Address> address = resolver.findAddressForLocation(latitude, longitude);
 
         assertFalse(address.isPresent());
+        Mockito.verify(client, Mockito.times(1)).doHttpGet(anyString());
+    }
+
+    @Test
+    public void usingTheWrongCoordinates() throws IOException, URISyntaxException, ParseException {
+        double latitude = 41.634159;
+        double longitude =  -9.65795;
+
+        String response = "{\"info\":{\"statuscode\":0,\"copyright\":{\"text\":\"© 2022 MapQuest, Inc.\",\"imageUrl\":\"http://api.mqcdn.com/res/mqlogo.gif\",\"imageAltText\":\"© 2022 MapQuest, Inc.\"},\"messages\":[]},\"options\":{\"maxResults\":1,\"ignoreLatLngInput\":false},\"results\":[{\"providedLocation\":{\"latLng\":{\"lat\":40.634159,\"lng\":-8.65795}},\"locations\":[{\"street\":\"Avenida João Jacinto de Magalhães\",\"adminArea6\":\"Aveiro\",\"adminArea6Type\":\"Neighborhood\",\"adminArea5\":\"Aveiro\",\"adminArea5Type\":\"City\",\"adminArea4\":\"Aveiro\",\"adminArea4Type\":\"County\",\"adminArea3\":\"\",\"adminArea3Type\":\"State\",\"adminArea1\":\"PT\",\"adminArea1Type\":\"Country\",\"postalCode\":\"3810-149\",\"geocodeQualityCode\":\"B1AAA\",\"geocodeQuality\":\"STREET\",\"dragPoint\":false,\"sideOfStreet\":\"N\",\"linkId\":\"0\",\"unknownInput\":\"\",\"type\":\"s\",\"latLng\":{\"lat\":40.63416,\"lng\":-8.65795},\"displayLatLng\":{\"lat\":40.63416,\"lng\":-8.65795},\"mapUrl\":\"\",\"roadMetadata\":{\"speedLimitUnits\":\"mph\",\"speedLimit\":31,\"tollRoad\":null}}]}]}";
+
+        Mockito.when(client.doHttpGet(anyString())).thenReturn(response);
+
+        Optional<Address> address = resolver.findAddressForLocation(latitude, longitude);
+
+        assertTrue(address.isPresent());
+        assertEquals("Avenida João Jacinto de Magalhães", address.get().getRoad());
         Mockito.verify(client, Mockito.times(1)).doHttpGet(anyString());
     }
 }
--- lab2/gs-mockForHttpClient/src/test/java/integration/AddressResolverIT.java
+++ lab2/gs-mockForHttpClient/src/test/java/integration/AddressResolverIT.java
@@ -1,5 +1,6 @@
 package integration;
 
+import connection.ISimpleHttpClient;
 import connection.TqsBasicHttpClient;
 import geocoding.Address;
 import geocoding.AddressResolver;
@@ -9,32 +10,56 @@ import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
+import java.util.NoSuchElementException;
+import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class AddressResolverIT {
 
+    private ISimpleHttpClient client;
+    private AddressResolver resolver;
 
     @BeforeEach
     public void init(){
+        client = new TqsBasicHttpClient();
+        resolver = new AddressResolver(client);
     }
 
     @Test
     public void whenGoodCoordidates_returnAddress() throws IOException, URISyntaxException, ParseException {
 
-        //todo
+        double latitude = 40.634159;
+        double longitude =  -8.65795;
 
-        // repeat the same tests conditions from AddressResolverTest, without mocks
+        Address result = resolver.findAddressForLocation(latitude, longitude).get();
 
+        Address expected = new Address("Avenida João Jacinto de Magalhães", "Aveiro", "Aveiro", "Portugal", "3810-149");
+
+        assertEquals(expected.getRoad(), result.getRoad());
     }
 
     @Test
     public void whenBadCoordidates_thenReturnNoValidAddrress() throws IOException, URISyntaxException, ParseException {
+        double latitude = 420;
+        double longitude = 69;
 
-        //todo
-        // repeat the same tests conditions from AddressResolverTest, without mocks
-        
+        Optional<Address> address = resolver.findAddressForLocation(latitude, longitude);
+
+        assertThrows(NoSuchElementException.class, () -> address.get().getRoad());
+    }
+
+    @Test
+    public void usingTheWrongCoordinates() throws IOException, URISyntaxException, ParseException {
+        double latitude = 41.634159;
+        double longitude =  -9.65795;
+
+        Address result = resolver.findAddressForLocation(latitude, longitude).get();
+
+        Address expected = new Address("Avenida João Jacinto de Magalhães", "Aveiro", "Aveiro", "Portugal", "3810-149");
+
+        assertNotEquals(expected.getRoad(), result.getRoad());
+
     }
 
 }
